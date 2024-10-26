8eff15802ec6391ab2b10471910454f85beedc7a
Ghrushnesh Rathod <ghrushneshr25@gmail.com>
2024-10-26 18:44:41 +0530

    added test cases for split circular linked list

---
+++ linkedlist/splitCircularLinkedList_test.go
@@ -0,0 +1,45 @@
+package linkedlist
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type SplitCircularLinkedListTestSuite struct {
+	suite.Suite
+}
+
+func TestSplitCircularLinkedList(t *testing.T) {
+	suite.Run(t, new(SplitCircularLinkedListTestSuite))
+}
+
+func (scll *SplitCircularLinkedListTestSuite) TestCase() {
+	tests := []struct {
+		head    []int
+		listOne []int
+		listTwo []int
+	}{
+		{
+			head:    []int{1, 2, 3, 4, 5, 6, 7, 8},
+			listOne: []int{1, 2, 3, 4},
+			listTwo: []int{5, 6, 7, 8},
+		},
+		{
+			head:    []int{1, 2, 3},
+			listOne: []int{1, 2},
+			listTwo: []int{3},
+		},
+	}
+
+	for index, test := range tests {
+		testName := fmt.Sprintf("Test Case :%v", index+1)
+		scll.Run(testName, func() {
+			mtslll := NewSplitCircularLinkedList(test.head)
+			mtslll.Execute()
+			scll.Equal(test.listOne, mtslll.ResultOne)
+			scll.Equal(test.listTwo, mtslll.ResultTwo)
+		})
+	}
+}
